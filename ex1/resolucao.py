b60e0c4835e8512d45a5fb2b6c5e98508c3c4513
André Luís <andreemoraees.borges@gmail.com>
2019-12-17 20:42:51 -0300

    resolucao ex 1

---
+++ ex1/resolucao.py
@@ -0,0 +1,9 @@
+nome = input('Digite seu nome: ')
+
+lista = ['andre', 'andre', 'andre', 'andre', 'andre', 'andre', 'andre']
+
+contador = 1
+
+while contador <= 7:
+    print(lista)
+    contador = contador + 1
